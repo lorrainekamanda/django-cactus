09ed62897dffabed3263ac1a6e7320dcf0daf3ca
Rain <user@localhost.localdomain>
2022-07-23 10:44:07 -0900

    adding Puzzle model

--- xword_data/models.py
+++ xword_data/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Puzzle(models.Model):
+
+    # Fields
+    title = models.CharField(max_length=255, blank=True)
+    date = models.DateField(auto_now_add=True,blank=False)
+    byline = models.CharField(max_length=255, blank=False)
+    publisher = models.CharField(max_length=12, blank=False)
+    
+   
+    # Model Functions
+    def __str__(self):
+        return str(self.publisher)
+
+
