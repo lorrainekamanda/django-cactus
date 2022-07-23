f6e1ace74ef00a3f9da83ab9284f06dd54924864
Rain <user@localhost.localdomain>
2022-07-23 10:51:14 -0900

    adding Entry model

--- xword_data/models.py
+++ xword_data/models.py
@@ -9,9 +9,13 @@ class Puzzle(models.Model):
     byline = models.CharField(max_length=255, blank=False)
     publisher = models.CharField(max_length=12, blank=False)
     
-   
-    # Model Functions
     def __str__(self):
         return str(self.publisher)
 
 
+class Entry(models.Model):
+
+    entry_text = models.CharField(max_length=50,blank=False,unique=True)
+
+    def __str__(self):
+        return str(self.entry_text)
